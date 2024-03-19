8a3293892c32dc0e8b820771fef87d22d7f7ab07
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2024-03-19 19:58:31 +0600

    Div.3, A done

---
+++ Codeforces/Round 935 (Div. 3)/Contest 1/B_Fireworks.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long a,b,m;
+        cin>>a>>b>>m;
+        long long count=0;
+        while(true)
+        {
+            long long x = a
+        }
+    }
+    return 0;
+}
